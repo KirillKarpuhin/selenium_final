a51fafb2ea4b2ebdb3d5fc8d483af34a0b4f427d
KirillKarpuhin <k-rreeaall@mail.ru>
2025-03-14 18:21:41 +0300

    обновлен поиск элементов

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,4 @@
-
+from selenium.common.exceptions import NoSuchElementException 
 
 # базовая страница, от которой наследуются остальные классы и опишем вспомогательные методы для работы с драйвером.
 class BasePage():
@@ -14,3 +14,22 @@ class BasePage():
 
     def open(self):
         self.browser.get(self.url)
+
+    # Напишем вспомогательный метод поиска элемента в нашей базовой странице BasePage, который будет возвращать нам True или False    
+    def __init__(self, browser, url, timeout=10): # В конструктор BasePage добавим команду для неявного ожидания со значением по умолчанию в 10
+        self.browser = browser
+        self.url = url
+        self.browser.implicitly_wait(timeout) 
+     
+    #  реализуем метод is_element_present, в котором будем перехватывать исключение.
+    #  В него будем передавать два аргумента: как искать (css, id, xpath и тд) и собственно что искать (строку-селектор). 
+    #  Чтобы перехватывать исключение, нужна конструкция try/except: 
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException): # если не найдет кнопку, например, логина, то вернет сообщение об ошибке из main_page
+            return False
+        return True
+    
+    # Чтобы импортировать нужное нам исключение, в самом верху файла нужно указать: 
+    #from selenium.common.exceptions import имя_исключения
--- pages/main_page.py
+++ pages/main_page.py
@@ -15,3 +15,8 @@ class MainPage(BasePage):
         # self.browser обращается так же как аргумент класса BasePage
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
+
+    # Чтобы выводить адекватное сообщение об ошибке, мы будем все проверки осуществлять с помощью assert и перехватывать исключения.
+    def should_be_login_link(self):
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
+
